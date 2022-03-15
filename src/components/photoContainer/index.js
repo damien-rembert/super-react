449f4326b5d131941b70a10675e4b0db6f6508b8
Damien R <damien.rembert@gmail.com>
2022-03-15 18:12:33 +0000

    loading photo api

---
+++ src/components/photoContainer/index.js
@@ -0,0 +1,9 @@
+export const PhotoContainer = ({ photo }) => {
+    return (
+      <>
+        <p>{photo.author}</p>
+        <img src={photo.download_url} alt="lorem picsum random" />
+      </>
+    );
+  };
+  
