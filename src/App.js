ea60cd46ddb84e60a4770de875c8c8f4c09f151b
Damien R <damien.rembert@gmail.com>
2022-03-15 18:08:18 +0000

    move login to login component

--- src/App.js
+++ src/App.js
@@ -1,42 +1,22 @@
 //@ts-check
 
-import React from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Login } from "./components/login";
+import { Home } from "./components/home";
+import { tokenLogin } from "./utils";
 import "./App.css";
 
 const App = () => {
   const [user, setUser] = useState();
-  const [username, setUsername] = useState();
-  const [email, setEmail] = useState();
-  const [pass, setPass] = useState();
-  const [bool, setBool] = useState(false);
-
-  const submitHandler = (e) => {
-    e.preventDefault();
-    setUser({ username: username, email: email, pass: pass });
-  };
+  useEffect(() => {
+    tokenLogin(setUser);
+  }, []);
 
   return (
     <div className="App">
-      {user && <h1>{user.username}</h1>}
-      {user ? <h1>{user.username}</h1> : <h1>Please type something</h1>}
-      <Login
-        handler={submitHandler}
-        nameSetter={setUsername}
-        emailSetter={setEmail}
-        passSetter={setPass}
-        boolSetter={setBool}
-        bool={bool}
-      />
-      <Login
-        handler={submitHandler}
-        nameSetter={setUsername}
-        emailSetter={setEmail}
-        passSetter={setPass}
-        boolSetter={setBool}
-        bool={bool}
-      />
+      {user && <h1>{user}</h1>}
+      {user ? <h1>{user}</h1> : <h1>Please type something</h1>}
+      {!user ? <Login setUser={setUser} /> : <Home />}
     </div>
   );
 };
--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,35 +1,44 @@
-//@ts-check
-import React from "react";
+import { useState } from "react";
+import { createUser, login } from "../../utils";
 
-export const Login = ({
-    handler,
-    nameSetter,
-    emailSetter,
-    passSetter,
-    boolSetter,
-    bool,
-  }) => {
-    return (
-      <>
-        <form onSubmit={handler}>
-          <input
-            onChange={(e) => nameSetter(e.target.value)}
-            placeholder="Username"
-          />
-          {!bool && (
-            <input
-              onChange={(e) => emailSetter(e.target.value)}
-              placeholder="Email"
-            />
-          )}
-          <input
-            onChange={(e) => passSetter(e.target.value)}
-            placeholder="Password"
-          />
-          <button type="submit">Submit</button>
-        </form>
-        <button onClick={() => boolSetter(!bool)}>Log-in or Sign-up</button>
-    </>
-    );
+export const Login = ({ setUser }) => {
+  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
+  const [pass, setPass] = useState();
+  const [bool, setBool] = useState(false);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    // setUser({ username: username, email: email, pass: pass });
+    if (bool) {
+      login(username, pass, setUser);
+    } else if (email && email.includes("@")) {
+      createUser(username, email, pass, setUser);
+    }
   };
-  
+
+  return (
+    <>
+      <form onSubmit={submitHandler}>
+        <input
+          onChange={(event) => setUsername(event.target.value)}
+          placeholder="Username"
+        />
+        {!bool && (
+          <input
+            onChange={(event) => setEmail(event.target.value)}
+            placeholder="Email"
+            type="email"
+          />
+        )}
+        <input
+          onChange={(event) => setPass(event.target.value)}
+          placeholder="Password"
+          type="password"
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <button onClick={() => setBool(!bool)}>Log-in or Sign-up</button>
+    </>
+  );
+};
